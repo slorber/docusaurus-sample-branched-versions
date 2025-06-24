922c15cf7e43f66779dc8c40962ae15ae97d06c8
Felipe Santos <felipecassiors@gmail.com>
2025-06-24 19:16:42 -0300

    Automate generation of versioned_docs

---
+++ prepare_versioned_docs.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+#!/bin/bash
+
+set -euxo pipefail
+
+branches=("v1" "v2")
+
+refs_to_fetch=()
+
+for branch in "${branches[@]}"; do
+  branch_ref="refs/heads/${branch}"
+  if ! git rev-parse --verify --quiet "${branch_ref}" > /dev/null; then
+    refs_to_fetch+=("${branch_ref}:refs/remotes/origin/${branch}")
+  fi
+done
+
+if [[ "${#refs_to_fetch[@]}" -gt 0 ]]; then
+  git fetch --no-tags origin "${refs_to_fetch[@]}"
+fi
+
+for branch in "${branches[@]}"; do
+  worktree_dir="versioned_docs/version-${branch}"
+  rm -rf "${worktree_dir}"
+  git worktree add "${worktree_dir}" "origin/${branch}" --detach --force
+done
+
+printf '%s\n' "${branches[@]}" | jq -R . | jq -s . | tee versions.json
